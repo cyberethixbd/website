52edccd5705ec4cfe77a714789ecb75bda8e152e
Cyber Ethix BD <atarif676@gmail.com>
2025-05-31 20:50:49 +0600

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,3 +1,26 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to CyberEthix BD</h1>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Home;
+
+
+
+
+
+
+
 import React from "react";
 import { Link } from "react-router-dom";
 
