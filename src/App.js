09450b83df6c16ff4fe53b63a32e26dd3f5fdc4e
Cyber Ethix BD <atarif676@gmail.com>
2025-05-31 20:23:48 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import Home from "./Home";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
 
 
 
