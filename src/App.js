cbec27d8651ef54240d9c061ad2d5a097627f813
Cyber Ethix BD <atarif676@gmail.com>
2025-05-31 20:20:29 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Home from "./Home";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+
 import React, { useState, useEffect } from "react";
 import { auth } from "./firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
